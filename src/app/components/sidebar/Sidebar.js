d195e068b46c31e7af7a391cfcc1b5b636146342
Max Kjellqvist <max.kj@outlook.com>
2021-02-01 13:58:24 +0100

    Added about me

--- src/app/components/sidebar/Sidebar.js
+++ src/app/components/sidebar/Sidebar.js
@@ -1,3 +1,4 @@
+import About from '../About/About';
 import Skills from '../Skills/Skills';
 import image from './profile-img.jpg';
 import './Sidebar.css';
@@ -10,12 +11,18 @@ function Sidebar() {
       </div>
       
       <div className="sidebar__links">
-        <a target="_blank" href="https://www.linkedin.com/in/max-kjellqvist-johnsson-57001559/">Linkedin</a>
-        <a target="_blank" href="https://github.com/Mkayy90/CV">Github</a>
+        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/max-kjellqvist-johnsson-57001559/">Linkedin</a>
+        <a target="_blank" rel="noreferrer" href="https://github.com/Mkayy90/CV">Github</a>
       </div>
 
       <div className="sidebar__skills">
+        <div className="sidebar__title">Skills</div>
         <Skills />
+      </div>
+
+      <div className="sidebar__about">
+      <div className="sidebar__title">About me</div>
+        <About />
       </div>
     </div>
   );
--- src/app/config.js
+++ src/app/config.js
@@ -105,17 +105,11 @@ export const skills = {
 };
 
 export const about = [
-  {
-    'title': 'About me',
-    'me': [
-      {
-        'title': 'Life',
-        'description': 'Live in Lund with my fiance & dog. Love tech in all forms. Love nature & hiking.',
-      },
-      {
-        'title': 'Work',
-        'description': 'I strive to write both good looking, performant and understandable code. Every now and then I try out the latest in the front-end world, like Svelte.',
-      },
-    ]
-  }
+  { 'title': 'Location', 'description': `Lund, Sweden` },
+  { 'title': 'Family', 'description': `Fiance & dog` },
+  { 'title': 'Interests', 'description': `#tech, #web, #nature, #gaming, #boardgames` },
+  { 'title': 'Me', 'description': `Front-end developer in his young 30s. Calm & rational. Prefer being sure over being hasty.` },
+  { 'title': 'At work', 'description': `Love minimalistic and pure code, and always strive for readable & performant code. Big passion for the User Experience.` },
+  { 'title': 'Contact', 'description': `max.kj@outlook.com` },
+  { 'title': '', 'description': `+46 702627803` }
 ];
